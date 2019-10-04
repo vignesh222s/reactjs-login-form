69dc125f9136cc0e1df3130359e4c84583d98178
vignesh222s <iamvigneshsundaram@gmail.com>
2019-10-04 15:46:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from "./components/form"
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Form />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/form.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+           form:"login"
+    }
+this.toggle={
+   login:"register",
+   register:"login"
+}
+
+}
+ onSubmit=(e) =>{
+    e.preventDefault();
+  }
+  onClick=()=>{
+    this.setState({form:this.toggle[this.state.form]})
+  }
+  render(){
+    return(
+            <div>
+            <h1> Login And Register Form </h1>
+              <div className="container">
+        <div style={{transform: `translate(${this.state.form === 'login' ? 0 : 250}px, 0px)`}} className="form-div">
+          <form onSubmit={this.onSubmit}>
+            <input placeholder="Email" type="text" />
+            <br/>
+            <input placeholder="Password" type="password" />
+            {this.state.form === 'login' ? '': <input placeholder="Re-typed password" type="password" />}
+            <button className="button-primary">Submit</button>
+          </form>
+        </div>
+        <div style={{transform: `translate(${this.state.form === 'login' ? 0 : -250}px, 0px)`}} className="button-div">
+          <p>{this.state.form === 'login' ? 'Do not have an account?' : 'Already a member?'}</p>
+          <button onClick={this.onClick.bind(this)}>{this.toggle[this.state.form]}</button>
+        </div>
+      </div>
+
+            </div>
+
+      )
+  }
+}
+export default App;
