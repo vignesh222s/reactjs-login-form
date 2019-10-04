dfe316bb09bd64fc7943f1892253a9de203bcdfb
vignesh222s <iamvigneshsundaram@gmail.com>
2019-10-04 16:03:40 +0530

    Updates

---
+++ precache-manifest.5474a2f123b535f18dcedd205938a5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e755978379f3fc8d083d2928ee20a3",
+    "url": "/reactjs-login-form/index.html"
+  },
+  {
+    "revision": "6627a44d7be79d8243b3",
+    "url": "/reactjs-login-form/static/css/main.0989f10c.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/reactjs-login-form/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "6627a44d7be79d8243b3",
+    "url": "/reactjs-login-form/static/js/main.dbfe7686.chunk.js"
+  },
+  {
+    "revision": "435c01a03c5c4fd5b9b7",
+    "url": "/reactjs-login-form/static/js/runtime-main.0528818e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactjs-login-form/static/media/logo.25bf045c.svg"
+  }
+]);
