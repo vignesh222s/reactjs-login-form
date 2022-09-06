7be44af6bd24c303a00c2e441f12eeeca3c2ddd3
vignesh222 <52827001+vignesh222@users.noreply.github.com>
2022-09-06 18:35:31 +0530

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-class App extends React.Component{
+export default class App extends React.Component{
   constructor(){
     super();
     this.state={
@@ -44,4 +44,3 @@ this.toggle={
       )
   }
 }
-export default App;
